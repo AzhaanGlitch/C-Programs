671b9ee37c78804a59f91aedbf550cd0d9b62683
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-09-03 16:20:24 +0530

    7th code added

---
+++ 07. Area of Triangle (Basic).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float b,h,Area;
+printf("The base of the triangle is: \n");
+scanf("%f",&b);
+printf("The height of the rectangle is: \n");
+scanf("%f",&h);
+Area=(b*h)/2;
+printf("The area of triangle is:%f \n", Area);
+return 0;
+}
