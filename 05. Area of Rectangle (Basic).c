96321080bf7a0a5e83f9e30aadc5c70df7b45f64
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-09-03 16:19:30 +0530

    5th code added

---
+++ 05. Area of Rectangle (Basic).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float l,b,Area;
+printf("The length of the rectangle is \n");
+scanf("%f",&l);
+printf("The breadth of the rectangle is \n");
+scanf("%f",&b);
+Area=l*b;
+printf("The area of rectangle is:%f \n",Area);
+return 0;
+}
