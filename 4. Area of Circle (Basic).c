91ab3ad72c4c597bc4c2622cf46758ed2fd4d127
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-09-03 16:27:27 +0530

    rearraned

--- 4. Area of Circle (Basic).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-float r, Area;
-printf("The radius of the circle is: \n");
-scanf("%f",&r);
-Area=3.14*r*r;
-printf("The area of the circle is:%f \n",Area);
-return 0;
-}
--- 6. Area of Square (Basic).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-int a,Area;
-printf("Enter the length of side of square: \n");
-scanf("%d", &a);
-Area=a*a;
-printf("The area of the square is:%d \n",Area);
-return 0;
-}
