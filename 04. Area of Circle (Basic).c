2c83d52ae4112432966cf65887c1b6dc8f6b6af6
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-09-03 16:18:46 +0530

    4th code updated

---
+++ 04. Area of Circle (Basic).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+float r, Area;
+printf("The radius of the circle is: \n");
+scanf("%f",&r);
+Area=3.14*r*r;
+printf("The area of the circle is:%f \n",Area);
+return 0;
+}
